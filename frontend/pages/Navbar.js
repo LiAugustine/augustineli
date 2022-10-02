27e43c7840a57d82b3708c95e2c01c740f965364
ali13 <ali13@student.gsu.edu>
2022-10-01 22:19:40 -0400

    updated navbar

--- frontend/pages/Navbar.js
+++ frontend/pages/Navbar.js
@@ -27,72 +27,73 @@ const Navbar = () => {
 
     return (
         <div className="Navbar">
+            <Center>
+                <Flex minWidth='max-content' alignItems='center' gap='2'>
+                    <Box>
+                        <Heading size='md' as='span'>Augustine Li</Heading>
+                    </Box>
+                    <ButtonGroup>
 
-            <Flex minWidth='max-content' alignItems='center' gap='2'>
-                <Box>
-                    <Heading size='md' as='span'>Augustine Li</Heading>
-                </Box>
-                <ButtonGroup>
+                        <Button variant='ghost' onClick={() => router.push('/')}>Home</Button>
+                        <Button variant='ghost' onClick={() => router.push('/Blog')}>Blog</Button>
+                        <Button variant='ghost' onClick={() => router.push('/Projects')}>Projects</Button>
+                    </ButtonGroup>
+                    <Spacer />
 
-                    <Button variant='ghost' onClick={() => router.push('/')}>Home</Button>
-                    <Button variant='ghost' onClick={() => router.push('/Blog')}>Blog</Button>
-                    <Button variant='ghost' onClick={() => router.push('/Projects')}>Projects</Button>
-                </ButtonGroup>
-                <Spacer />
+                    {loggedIn ? (
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button>
+                                    <Wrap>
+                                        <WrapItem>
+                                            <Avatar size='sm' src={loggedIn.picture} />
+                                        </WrapItem>
+                                    </Wrap>
+                                    {loggedIn.name}
+                                </Button>
+                            </PopoverTrigger>
+                            <Portal>
+                                <PopoverContent>
+                                    <PopoverArrow />
+                                    <PopoverHeader>Account Options</PopoverHeader>
+                                    <PopoverCloseButton />
+                                    <PopoverBody>
+                                        <a href='/logout'>
+                                            <Button colorScheme='red' size='md'>
+                                                Sign-out
+                                            </Button>
+                                        </a>
+                                    </PopoverBody>
 
-                {loggedIn ? (
-                    <Popover>
-                        <PopoverTrigger>
-                            <Button>
-                                <Wrap>
-                                    <WrapItem>
-                                        <Avatar size='sm' src={loggedIn.picture} />
-                                    </WrapItem>
-                                </Wrap>
-                                {loggedIn.name}
-                            </Button>
-                        </PopoverTrigger>
-                        <Portal>
-                            <PopoverContent>
-                                <PopoverArrow />
-                                <PopoverHeader>Account Options</PopoverHeader>
-                                <PopoverCloseButton />
-                                <PopoverBody>
-                                    <a href='/logout'>
-                                        <Button colorScheme='red' size='md'>
-                                            Sign-out
-                                        </Button>
-                                    </a>
-                                </PopoverBody>
+                                </PopoverContent>
+                            </Portal>
+                        </Popover>
+                    ) : (
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button colorScheme='green' size='md'>Sign-in</Button>
+                            </PopoverTrigger>
+                            <Portal>
+                                <PopoverContent>
+                                    <PopoverArrow />
+                                    <PopoverHeader>Sign-in Options</PopoverHeader>
+                                    <PopoverCloseButton />
+                                    <PopoverBody>
+                                        <a href='/login'>
+                                            <Button colorScheme='gray' color='black' size='md'>
+                                                <FcGoogle /> Sign-in with Google
+                                            </Button>
+                                        </a>
+                                    </PopoverBody>
 
-                            </PopoverContent>
-                        </Portal>
-                    </Popover>
-                ) : (
-                    <Popover>
-                        <PopoverTrigger>
-                            <Button colorScheme='green' size='md'>Sign-in</Button>
-                        </PopoverTrigger>
-                        <Portal>
-                            <PopoverContent>
-                                <PopoverArrow />
-                                <PopoverHeader>Sign-in Options</PopoverHeader>
-                                <PopoverCloseButton />
-                                <PopoverBody>
-                                    <a href='/login'>
-                                        <Button colorScheme='gray' color='black' size='md'>
-                                            <FcGoogle /> Sign-in with Google
-                                        </Button>
-                                    </a>
-                                </PopoverBody>
-
-                            </PopoverContent>
-                        </Portal>
-                    </Popover>
-                )}
-            </Flex>
-
+                                </PopoverContent>
+                            </Portal>
+                        </Popover>
+                    )}
+                </Flex>
+            </Center>
             <Divider />
+            <br></br>
         </div>
     )
 }
